50900c4ab4226fe7186edeb40051184678f9e8b9
Neihtk2 <truongthienk2@gmail.com>
2024-09-12 08:44:10 +0700

    fixbugbutton

--- lib/utils/Drop_button.dart
+++ lib/utils/Drop_button.dart
@@ -1,83 +1,58 @@
 import 'package:fina/utils/color.dart';
 import 'package:flutter/material.dart';
 
-class DropdownStyledExample extends StatefulWidget {
+class PopupMenu extends StatefulWidget {
   @override
-  _DropdownStyledExampleState createState() => _DropdownStyledExampleState();
+  _PopupMenuState createState() => _PopupMenuState();
 }
 
-class _DropdownStyledExampleState extends State<DropdownStyledExample> {
-  String dropdownValue = 'Day'; // Giá trị mặc định ban đầu
+class _PopupMenuState extends State<PopupMenu> {
+  String _selectedValue = 'Day'; // Giá trị mặc định
+  final List<String> _options = ['Day', 'Week', 'Month'];
+
+  void _handleMenuItemSelected(String value) {
+    if (value == "Day") {
+      print('Thien');
+    } else if (value == "Week") {
+      print('Phuc');
+    } else {
+      print("Dev");
+    }
+    // Thực hiện hành động khi một mục được chọn
+
+    // Bạn có thể thêm logic tùy chỉnh của mình tại đây
+    // Ví dụ: show a dialog or perform some other action
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.symmetric(horizontal: 12),
-      decoration: BoxDecoration(
-        border: Border.all(color: themColor, width: 2),
-        borderRadius: BorderRadius.circular(12), // Bo viền
-      ),
-      child: DropdownButtonHideUnderline(
-        child: DropdownButton(
-          value: dropdownValue,
-          icon: Icon(Icons.arrow_drop_down), // Icon thả xuống
-          iconSize: 24,
-          elevation: 16,
-          style: TextStyle(color: Colors.deepPurple, fontSize: 16),
-          onChanged: (String? newValue) {
-            setState(() {
-              dropdownValue = newValue!;
-            });
-          },
-          items: [
-            DropdownMenuItem(
-              value: 'Day',
-              child: SizedBox(
-                width: 100,
-                child: ElevatedButton(
-                  onPressed: () {
-                    print('Day Button Pressed');
-                    Navigator.pop(context);
-                    setState(() {
-                      dropdownValue = 'Day';
-                    });
-                  },
-                  child: Text('Day'),
-                ),
-              ),
-            ),
-            DropdownMenuItem(
-              value: 'Week',
-              child: SizedBox(
-                width: 100,
-                child: ElevatedButton(
-                  onPressed: () {
-                    print('Week Button Pressed');
-                    Navigator.pop(context);
-                    setState(() {
-                      dropdownValue = 'Week';
-                    });
-                  },
-                  child: Text('Week'),
-                ),
-              ),
-            ),
-            DropdownMenuItem(
-              value: 'Month',
-              child: SizedBox(
-                width: 100,
-                child: ElevatedButton(
-                  onPressed: () {
-                    print('Month Button Pressed');
-                    Navigator.pop(context);
-                    setState(() {
-                      dropdownValue = 'Month';
-                    });
-                  },
-                  child: Text('Month'),
-                ),
-              ),
-            ),
+    return PopupMenuButton<String>(
+      onSelected: (String value) {
+        setState(() {
+          _selectedValue = value;
+        });
+        _handleMenuItemSelected(value); // Gọi hàm khi mục được chọn
+      },
+      itemBuilder: (BuildContext context) {
+        return _options.map((String value) {
+          return PopupMenuItem<String>(
+            value: value,
+            child: Text(value),
+          );
+        }).toList();
+      },
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 16.0),
+        decoration: BoxDecoration(
+          border: Border.all(color: themColor),
+          borderRadius: BorderRadius.circular(4.0),
+        ),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Text(_selectedValue),
+            SizedBox(width: 8),
+            Icon(Icons.arrow_drop_down),
           ],
         ),
       ),
